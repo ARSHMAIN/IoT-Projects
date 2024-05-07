68771ed5cbd5510a4a5ac830bcbf5cdbb7d73fd4
trevor-tam <trevortam99@gmail.com>
2024-05-06 20:44:05 -0400

    More css added
    a;sldkfj

--- IoT-Phase-4/hello.py
+++ IoT-Phase-4/hello.py
@@ -1,9 +1,7 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
 import dash_daq as daq
 from dash.dependencies import Input, Output
-from dash import html
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -14,21 +12,21 @@ app.layout = html.Div(
         html.Div(
             id='profile',
             children=[
-                html.P('Profile', style={'color': 'grey', 'textAlign': 'center', 'font-family': 'Verdana','fontSize': '40px'}),
+                html.P('Profile', style={'color': 'grey', 'textAlign': 'center', 'font-family': 'Verdana', 'fontSize': '40px'}),
                 html.Img(src='/assets/my_sunshine.jpg', style={'width': '150px', 'height': '150px', 'borderRadius': '50%', 'margin': '0 auto', 'display': 'block'}),
-                html.Label('User ID', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block'}),
+                html.Label('User ID', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block', 'font-family': 'Verdana'}),
                 dcc.Input(id='user-id', type='text', value='', style={'width': '100%', 'margin-bottom': '20px', 'height': '50px'}),
-                html.Label('Name', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block'}),
+                html.Label('Name', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block', 'font-family': 'Verdana'}),
                 dcc.Input(id='name', type='text', value='', style={'width': '100%', 'margin-bottom': '20px', 'height': '50px'}),
-                html.Label('Temp. Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block'}),
-                dcc.Input(id='temp-threshold', type='number', value=0, style={'width': '100%', 'margin-bottom': '20px', 'height': '50px'}),
-                html.Label('Humidity Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block'}),
-                dcc.Input(id='humidity-threshold', type='number', value=0, style={'width': '100%', 'margin-bottom': '20px', 'height': '50px'}),
-                html.Label('Light Intensity Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block'}),
+                html.Label('Temp. Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block', 'font-family': 'Verdana'}),
+                dcc.Input(id='temp-threshold', type='number', value=0, style={'width': '100%', 'margin-bottom': '20px', 'height': '50px', 'font-family': 'Verdana'}),
+                html.Label('Humidity Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block', 'font-family': 'Verdana'}),
+                dcc.Input(id='humidity-threshold', type='number', value=0, style={'width': '100%', 'margin-bottom': '20px', 'height': '50px', 'font-family': 'Verdana'}),
+                html.Label('Light Intensity Threshold', style={'color': 'grey', 'margin-bottom': '5px', 'display': 'block', 'font-family': 'Verdana'}),
                 dcc.Input(id='light-intensity-threshold', type='number', value=0, style={'width': '100%', 'margin-bottom': '20px', 'height': '50px'}),
-                html.Button('Submit Changes', id='submit-button', n_clicks=0, style={'width': '100%', 'height': '50px', 'backgroundColor': 'lightblue', 'margin-top': '20px'})
+                html.Button('Submit Changes', id='submit-button', n_clicks=0, style={'width': '100%', 'height': '50px', 'backgroundColor': 'lightblue', 'margin-top': '20px', 'font-family': 'Verdana'})
             ],
-            style={'flex': 1,'width': '20%', 'borderRadius': '10px', 'border': '5px ridge rgb(128, 0, 32)', 'padding': '20px', 'textAlign': 'center'}
+            style={'flex': 1, 'width': '20%', 'borderRadius': '10px', 'border': '5px ridge rgb(128, 0, 32)', 'padding': '20px', 'textAlign': 'center'}
         ),
 
         html.Div(
@@ -40,9 +38,8 @@ app.layout = html.Div(
                         html.Div(
                             id='phase2',
                             children=[
-                                html.P('Temperature and Humidity', style={'color': 'grey','textAlign': 'center', 'font-family': "Verdana"}),
                                 html.Div([
-                                    daq.Thermometer(
+                                        daq.Thermometer(
                                         id='temperature-gauge',
                                         label='Temperature',
                                         labelPosition='top',
@@ -51,7 +48,7 @@ app.layout = html.Div(
                                         value=0,
                                         min=-30,
                                         max=40,
-                                        style={'width': '50%', 'margin-right': '5%'}
+                                        style={'width': '50%', 'margin-right': '5%', 'color': 'grey', 'height': '100px'}
                                     ),
                                     daq.Gauge(
                                         color={
@@ -68,27 +65,48 @@ app.layout = html.Div(
                                         value=0,
                                         max=50,
                                         min=0,
-                                        style={'width': '50%'}
+                                        style={'width': '50%', 'color': 'grey', 'height': '100px'}
                                     )
-                                ], style={'display': 'flex', 'justifyContent': 'space-between'})
+
+
+                                ], style={'display': 'flex', 'justifyContent': 'space-between'}),
                             ],
                             style={'flex': 1, 'border': '5px ridge rgb(128, 0, 32)', 'padding': '20px'}
                         ),
                         html.Div(
                             id='phase3',
                             children=[
-                                html.P('LED and Fan', style={'color': 'grey','textAlign': 'center', 'font-family': "Verdana"}),
                                 html.Div([
-                                    html.Img(src='/assets/led_off.jpg', style={'width': '150px', 'height': '150px', 'borderRadius': '50%', 'margin-right': '90px', }),
-                                    html.Img(src='/assets/fan_off.png', style={'width': '150px', 'height': '150px', 'borderRadius': '50%', 'margin-left': '90px'})
-                                ], style={'display': 'flex', 'justifyContent': 'center'})
+                                    html.Div([
+                                        html.Div("Light Control", style={'color': 'grey', 'textAlign': 'center', 'margin-right': '175px', 'margin-bottom': '20px', 'font-family': 'Verdana'}),
+                                        html.Img(src='/assets/led_off.png', style={'width': '100px', 'height': '100px', 'borderRadius': '50%', 'margin-right': '175px'}),
+                                        daq.BooleanSwitch(
+                                            id='light-switch',
+                                            on=False,
+                                            style={'font-family': 'Verdana', 'color': 'green', 'margin-top': '20px', 'margin-right': '175px'}
+                                        ),
+                                        html.P('Light Intensity:', style={'color': 'grey', 'textAlign': 'center', 'font-family': 'Verdana', 'margin-right': '175px'}),
+                                        html.P('Light Status:', style={'color': 'grey', 'textAlign': 'center', 'font-family': 'Verdana', 'margin-right': '175px'}),
+                                    ], style={'display': 'inline-block'}),
+
+                                    html.Div([
+                                        html.Div("Fan Control", style={'color': 'grey', 'textAlign': 'center', 'margin-left': '175px', 'margin-bottom': '20px', 'font-family': 'Verdana'}),
+                                        html.Img(src='/assets/fan_off.png', style={'width': '100px', 'height': '100px', 'borderRadius': '50%', 'margin-left': '175px'}),
+                                        daq.BooleanSwitch(
+                                            id='fan-switch',
+                                            on=False,
+                                            style={'font-family': 'Verdana', 'color': 'green', 'margin-top': '20px', 'margin-left': '175px'}
+                                        ),
+                                        html.P('Fan Intensity:', style={'color': 'grey', 'textAlign': 'center', 'font-family': 'Verdana', 'margin-left': '175px'}),
+                                    ], style={'display': 'inline-block'})
+                                ], style={'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'})
                             ],
                             style={'flex': 1, 'border': '5px ridge rgb(128, 0, 32)', 'padding': '20px'}
                         ),
                         html.Div(
                             id='phase4',
                             children=[
-                                html.P('Bluetooth Devices', style={'color': 'grey','textAlign': 'center', 'font-family': "Verdana"})
+                                html.P(style={'color': 'grey', 'textAlign': 'center', 'font-family': "Verdana"})
                             ],
                             style={'flex': 1, 'border': '5px ridge rgb(128, 0, 32)'}
                         )
